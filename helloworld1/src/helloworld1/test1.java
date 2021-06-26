83519aba358c2f5da0c7da08f693f0d7f2bed963
mohangav <63729999+mohangav@users.noreply.github.com>
2021-06-26 19:25:05 +0530

    Fixed AB#1

--- helloworld1/src/helloworld1/test1.java
+++ helloworld1/src/helloworld1/test1.java
@@ -7,6 +7,7 @@ public class test1 {
 		System.out.println("Hello World 1");
 		System.out.println("This is Mohan");
 		System.out.println("This is ravan");
+		System.out.println("This is kans");
 	}
 
 }
